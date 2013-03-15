a81e3237b4cd1a52bba6ef1d8026676357fcc50a
Norbert Kawiński <kawa12321@gmail.com>
2013-03-15 22:44:36 +0100

    Added Tasks system

---
+++ Framework/TasksMgr.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+
+#include "TasksMgr.h"
+
+TasksMgr::~TasksMgr() 
+{
+	for(auto it : tasks)
+	{
+		delete it.first;  // TaskTimer*
+		delete it.second; // List<TaskData>*
+	}
+}
+
+void TasksMgr::run(float time)
+{
+	// get through all registered tasks
+	for(auto it : tasks)
+	{
+		if (time >= it.first->time) // has time passed limit?
+		{
+			it.first->time = it.first->interval; // reset counter
+			// get through all registered functions for this task
+			for(auto it2 = it.second->begin(), next = it2; it2 != it.second->end(); it2 = next)
+			{
+				++next;
+				(it2->executor)(&*it2); // calls a function stored in it2
+			}
+			/* TODO: check if it works
+			for(auto it2 = it.second->begin(), next = it2; it2 != it.second->end(); it2 = next++)
+				(it2->executor)(&*it2); // calls a function stored in it2
+			*/
+		}
+		else
+			it.first->time -= time;
+	}
+}
+
+TaskData* TasksMgr::registerTask(functor funcPointer, float interval)
+{
+	TaskData data(interval, funcPointer);
+
+	// search a good place to put this task
+	for (auto it = tasks.begin(); it != tasks.end(); ++it)
+	{
+		if (it->first->interval == interval)
+		{
+			funcs list = it->second;
+			data.listPtr = list;
+			list->push_back(data);
+			list->rbegin()->ptr = --list->rbegin().base();
+			return &*list->rbegin();
+		}
+	}
+
+	// if good place not found, add new place
+	funcs v = new std::list<TaskData>();
+	v->push_back(data);
+	tasks.push_back(std::make_pair(new TaskTimer(interval, 0.f), v));
+
+	auto datait = tasks.rbegin()->second->begin();
+	datait->listPtr = tasks.rbegin()->second;
+	datait->ptr = datait;
+	return &*datait;
+}
+
+void TasksMgr::removeTask(TaskData *td)
+{
+	td->listPtr->erase(td->ptr);
+}
---
+++ Framework/TasksMgr.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <vector>
+#include <boost\function.hpp>
+#include <list>
+
+class TaskTimer
+{
+public:
+	TaskTimer(float _interval, float _time) : interval(_interval), time(_time)
+	{}
+
+	float interval; // how much time to wait before run the task
+	float time; // how much time passed
+};
+
+class TaskData
+{
+private:
+	typedef std::function<void(TaskData*)> functor;
+	typedef std::list<TaskData>* funcs;
+	TaskData(float _interval, functor _executor) : interval(_interval), executor(_executor)
+	{}
+
+	funcs listPtr;
+	std::list<TaskData>::iterator ptr;
+	float interval;
+	functor executor;
+
+	friend class TasksMgr; // Only Tasks needs using this...
+};
+
+class TasksMgr
+{
+private:
+	typedef std::function<void(TaskData*)> functor;
+	typedef std::list<TaskData>* funcs;
+	typedef std::vector<std::pair<TaskTimer*, funcs>>::iterator iterator;
+	typedef std::list<TaskData>::iterator tasksIterator;
+
+public:
+	~TasksMgr();
+
+	// time - time passed since last tick
+	void run(float time);
+	TaskData* registerTask(functor funcPointer, float interval);
+	void removeTask(TaskData *td);
+
+private:
+	std::vector<std::pair<TaskTimer*, std::list<TaskData>*>> tasks;
+};
