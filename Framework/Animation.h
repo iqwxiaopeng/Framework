faba1e00d461d7703e293b4fbff1db58aab5e153
Damian <damiansht@gmail.com>
2013-03-15 21:40:59 +0100

    Animation reupload

---
+++ Framework/Animation.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "stdafx.h"
+class Displayable;
+
+class Animation
+{
+public :
+
+private :
+Displayable* frames;
+int frameWidth,frameHeight;
+int frameCountX,frameCountY;
+
+protected :
+};
