413d15b97d96c1fabd0769c42ab339681311461e
Damian <damiansht@gmail.com>
2013-03-15 21:50:44 +0100

    Animation update

--- Framework/Animation.cpp
+++ Framework/Animation.cpp
@@ -1,2 +1,24 @@
 #include "Animation.h"
 #include "stdafx.h"
+
+void Animation::SetAnimationTime(int time)
+{
+	animationTime = time;
+	timePerFrame = animationTime / frameCount;
+}
+
+void Animation::StartAnimation()
+{
+	currentTime = 0;
+}
+
+void Animation::StartAnimation(int time)
+{
+	SetAnimationTime(time);
+	currentTime = 0;
+}
+void Animation::StartAnimation(int time,int frame)
+{
+	SetAnimationTime(time);
+
+}
--- Framework/Animation.h
+++ Framework/Animation.h
@@ -6,11 +6,30 @@ class Displayable;
 class Animation
 {
 public :
+	Animation(Displayable img,int frameCountX,int frameCountY) :image(img)
+		//Starts animation functions
+		//Start Animation with current AnimationTime
+	void StartAnimation();
+	//Starts Animation and sets AnimationTime
+	void StartAnimation(int); 
+	//Arg1 - AnimationTime
+	//Arg2 - from which frame does animation start
+	void StartAnimation(int,int); 
+
+
+	void StopAnimation(); //Return to 0 frame.
+	void SetAnimationTime(int);
+	//Pause/Unpause animation. Returns the actual state of Pausing
+	bool PauseAnimation();
+
+
+
+	bool pause;
 
 private :
-Displayable* frames;
+Displayable image;
 int frameWidth,frameHeight;
-int frameCountX,frameCountY;
-
+int frameCountX,frameCountY,frameCount;
+int animationTime,currentTime,timePerFrame;
 protected :
 };
