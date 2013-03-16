d34267b5d35fee3b9e3bd610210a69dabde1ff47
Damian <damiansht@gmail.com>
2013-03-16 20:53:19 +0100

    Particle master done?

---
+++ Framework/ParticleMaster.cpp
@@ -0,0 +1,46 @@
+#include "ParticleMaster.h"
+
+ParticleMaster::ParticleMaster()
+{
+}
+
+void ParticleMaster::Add(Particle *part)
+{
+	particles.push_back(part);
+}
+
+void ParticleMaster::Delete(int index)
+{
+	delete particles[index];
+	particles[index] = particles[particles.size()-1];
+	particles.pop_back();
+}
+
+void ParticleMaster::Delete(Particle *wut)
+{
+	for(int i = 0;i < particles.size();i++)
+		if(particles[i] == wut)
+		{
+			Delete(i);
+			return;
+		}
+}
+
+void ParticleMaster::Step()
+{
+	if(pause)return;
+	for(int i = 0;i < particles.size();i++)
+	{
+		particles[i]->Step();
+		if(particles[i]->End())
+		Delete(i);
+	}
+}
+
+void ParticleMaster::Render(sf::RenderWindow *pRW)
+{
+	for(int i = 0; i < particles.size();i++)
+	{
+		particles[i]->Render(pRW);
+	}
+}
