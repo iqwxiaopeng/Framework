621bba4ce5ad4d6aec93a37ec199fc5048eac7fc
Damian <damiansht@gmail.com>
2013-03-16 01:21:27 +0100

    Animation update
    Big progress. Everything (almost?) done.

--- Framework/Animation.cpp
+++ Framework/Animation.cpp
@@ -1,5 +1,6 @@
-#include "Animation.h"
 #include "stdafx.h"
+#include "Animation.h"
+
 
 void Animation::SetAnimationTime(int time)
 {
@@ -7,18 +8,63 @@ void Animation::SetAnimationTime(int time)
 	timePerFrame = animationTime / frameCount;
 }
 
-void Animation::StartAnimation()
+void Animation::Start()
 {
+	if(animationTime == 0)
+		return;
 	currentTime = 0;
+	pause = false;
 }
 
-void Animation::StartAnimation(int time)
+void Animation::Start(int time)
 {
 	SetAnimationTime(time);
 	currentTime = 0;
+	pause = false;
 }
-void Animation::StartAnimation(int time,int frame)
+void Animation::Start(int time,int frame)
 {
 	SetAnimationTime(time);
+	SetAnimationFrame(frame);
+	pause = false;
+}
 
+void Animation::SetAnimationFrame(int frame)
+{
+	currentTime = timePerFrame * frame;
+	currentFrame = frame;
+}
+
+bool Animation::Pause()
+{
+	pause=pause?false:true;
+	return pause;
+}
+
+void Animation::Tick(float frameTime)
+{
+	if(pause)return;
+	currentTime++;
+	currentFrame = currentFrame / timePerFrame;
+	currentTime %= animationTime;
+	SetFrameBounds();
+}
+
+void Animation::SetFrameBounds()
+{
+	int frameX = currentFrame % frameCountX;
+	int frameY = currentFrame / frameCountX;
+	image.sprite.setTextureRect(sf::IntRect( (frameWidth+spaceX) * frameX,(frameHeight+spaceY) * frameY,frameWidth,frameHeight)); //can be deleted, not sure.
+}
+
+void Animation::Render(sf::RenderWindow *pRW)
+{
+	pRW->draw(image.sprite);
+}
+
+void Animation::SetPosition(float x,float y)
+{
+	posX = x;
+	posY = y;
+	image.sprite.setPosition(x,y);
 }
--- Framework/Animation.h
+++ Framework/Animation.h
@@ -1,35 +1,54 @@
 #pragma once
 
-#include "stdafx.h"
-class Displayable;
+#include <SFML\Graphics.hpp>
+
+//placeholder, original one will be done by Castro12321
+class Displayable
+{
+	public :
+		sf::Sprite sprite;
+};
 
 class Animation
 {
 public :
-	Animation(Displayable img,int frameCountX,int frameCountY) :image(img)
+	Animation(Displayable img,int _frameCountX,int _frameCountY) :image(img),frameCountX(_frameCountX),frameCountY(_frameCountY),frameCount(_frameCountX*_frameCountY),
+		spaceX(0),spaceY(0),pause(true),animationTime(0)
+	{}
 		//Starts animation functions
 		//Start Animation with current AnimationTime
-	void StartAnimation();
+	void Start();
 	//Starts Animation and sets AnimationTime
-	void StartAnimation(int); 
+	void Start(int); 
 	//Arg1 - AnimationTime
 	//Arg2 - from which frame does animation start
-	void StartAnimation(int,int); 
+	void Start(int,int); 
 
-
-	void StopAnimation(); //Return to 0 frame.
+	void Stop(); //Return to 0 frame.
 	void SetAnimationTime(int);
+	void SetAnimationFrame(int);
+	void SetPosition(float,float);
+	//set spaces between frames.
+	void SetSpaces(int,int);
 	//Pause/Unpause animation. Returns the actual state of Pausing
-	bool PauseAnimation();
+	bool Pause();
 
-
+	//Frame time is now ignored.
+	void Tick(float frameTime);
+	void Render(sf::RenderWindow *pRW);
 
 	bool pause;
 
-private :
+protected :
+
 Displayable image;
+float posX,posY;
 int frameWidth,frameHeight;
 int frameCountX,frameCountY,frameCount;
-int animationTime,currentTime,timePerFrame;
-protected :
+int animationTime,currentTime,timePerFrame,currentFrame;
+int spaceX,spaceY;
+
+	void SetFrameBounds();
+
+private :
 };
