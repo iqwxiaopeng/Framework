7d2057f758cfc36985f8528adf62d30c8b2d5d86
Norbert Kawiński <kawa12321@gmail.com>
2013-03-16 21:46:17 +0100

    Added indent

--- Framework/ResourcesMgr.h
+++ Framework/ResourcesMgr.h
@@ -4,17 +4,19 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+// TODO: <soon> rewrite to handle sounds, etc
+
 class ResourcesMgr
 {
-public:
-	std::unordered_map<std::string, sf::Texture*> textures;
+	public:
+		std::unordered_map<std::string, sf::Texture*> textures;
 
-	void addTexture(std::string name);
+		void addTexture(std::string name);
 
-	ResourcesMgr();
-	~ResourcesMgr();
-private:
-	sf::Texture* getTexture(std::string filename);
-	void addObject(std::unordered_map<std::string, sf::Texture*>& map, std::string name);
+		ResourcesMgr();
+		~ResourcesMgr();
+	private:
+		sf::Texture* getTexture(std::string filename);
+		void addObject(std::unordered_map<std::string, sf::Texture*>& map, std::string name);
 };
 
