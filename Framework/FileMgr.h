98437151215f55de02910c9ff926c7c2676cb86a
Damian <damiansht@gmail.com>
2013-05-05 01:51:17 +0200

    increased readability of FileMgr ( Check it)
    I think that typedefinf this one improves readability of the code.

--- Framework/FileMgr.h
+++ Framework/FileMgr.h
@@ -5,14 +5,15 @@
 
 class FileMgr
 {
+	typedef std::vector<std::string> Lines;
 public:
 	static bool clearFile(std::string path);
 
 	static bool	write  (std::string path, std::string line);
-	static bool	write  (std::string path, std::vector<std::string> lines);
+	static bool	write  (std::string path, Lines lines);
 	static bool	writeln(std::string path, std::string text) { return write(path, text + "\n"); }
 
 	static int	search (std::string path, std::string text); // Found Text or not and how many (counter not implemented yet)
 
-	static std::vector<std::string> getFile(std::string path);
+	static Lines getFile(std::string path);
 };
